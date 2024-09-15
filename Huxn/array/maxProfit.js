ed08963e0f0c05d1d9cd1f381bd89ca643a7f418
Buomkuoth Yian <buom.nyajal@gmail.com>
2024-09-15 17:39:35 +0300

    Modified max profit function

--- Huxn/array/maxProfit.js
+++ Huxn/array/maxProfit.js
@@ -1,17 +1,19 @@
 // The function loops through the array of stock prices, tracking the minimum price encountered so far and updating the maximum profit whenever a higher profit is found. The final result returned by the function is the highest profit that can be made.
 
 function maxProfit(prices) {
-  let minPrice = Infinity;
+  let minPrice = prices[0];
   let maxProfit = 0;
 
-  for (let i = 0; i < prices.length; i++) {
-    if (prices[i] < minPrice) {
-      minPrice = prices[i];
-    } else if (prices[i] - minPrice > maxProfit) {
-      maxProfit = prices[i] - minPrice;
-    }
-  }
+  for (let i = 1; i < prices.length; i++) {
+    const currentPrice = prices[i];
 
+    // Update minimum price if the lower price is found
+    minPrice = Math.min(minPrice, currentPrice);
+
+    const potentialProfit = currentPrice - minPrice;
+
+    maxProfit = Math.max(maxProfit, potentialProfit);
+  }
   return maxProfit;
 }
 
